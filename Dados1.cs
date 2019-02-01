14d5e93b7ab8d5440096abc4182dcfe298c98f66
SebasARZ <47035779+SebasARZ@users.noreply.github.com>
2019-02-01 13:50:56 -0500

    Add files via upload

---
+++ Dados1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INICIO
+
+
+            Random dado = new Random();
+
+            int dado1 = dado.Next(1, 7);
+            int dado2 = dado.Next(1, 7);
+
+            int contador = 0;
+            int contador2 = 0;
+            int s = dado1 + dado2;
+
+            Console.WriteLine("Su puntuacion es =" + s);
+            Console.WriteLine("¿Desea continuar?");
+
+            string respuesta = (Console.ReadLine());
+
+            while(respuesta == "s")
+            {
+                int dado3 = dado.Next(1, 7);
+                int dado4 = dado.Next(1, 7);
+                int total = dado3 + dado4;
+                int acumulado = s + total;
+
+                Console.WriteLine("Dado1 = " + dado3);
+                Console.WriteLine("Dado2 = " + dado4);
+
+                s = acumulado;
+
+                if (acumulado <= 100 || ((dado3 == 1) && (dado4 == dado3))) { Console.WriteLine("Ha perdido");
+                    break;
+                }
+                int a = 0;
+                while(dado3 == dado1)
+                {
+                    a++;
+                }
+                if (a >= 3) { Console.WriteLine("Ha ganado");
+                    break;
+                }
+                if (total >= 6) contador2++;
+                contador++;
+            }Console.WriteLine("Fin del juego");
+            double tan = contador2 / contador;
+            Console.WriteLine("Su porcentaje cuando saco 6 =" + tan);
+
+        }
+    }
+}
---
+++ Validacion1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 2; i++)
+            {
+
+                Random random = new Random();
+                int a = random.Next(0, 11);
+                int b = random.Next(0, 11);
+
+                int capcha = a + b;
+
+                Console.WriteLine("Cuanto es: a+b");
+                Console.WriteLine("a =" + a);
+                Console.WriteLine("b =" + b);
+
+                int correcto = int.Parse(Console.ReadLine());
+
+                if (correcto == capcha)
+                {
+                    Console.WriteLine("Puede continuar");
+                    break;
+
+                }
+                if (i >= 2) Console.WriteLine("No tiene mas intentos");
+            }
+
+            
+
+        }
+    }
+}
