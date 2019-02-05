cbd51aca2c54d8fbc672ccc2d05899e635172005
SebasARZ <47035779+SebasARZ@users.noreply.github.com>
2019-02-05 09:59:22 -0500

    Add files via upload

--- Dados1.cs
+++ Dados1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dados1
+namespace Dados
 {
     class Program
     {
@@ -15,46 +15,58 @@ namespace Dados1
 
             Random dado = new Random();
 
-            int dado1 = dado.Next(1, 7);
-            int dado2 = dado.Next(1, 7);
+          
 
             int contador = 0;
             int contador2 = 0;
-            int s = dado1 + dado2;
+            int s = 0;
+            int a = 0;
 
-            Console.WriteLine("Su puntuacion es =" + s);
-            Console.WriteLine("¿Desea continuar?");
-
+            Console.WriteLine("Presione s para inciar");
             string respuesta = (Console.ReadLine());
 
-            while(respuesta == "s")
+            while (respuesta == "s")
             {
-                int dado3 = dado.Next(1, 7);
-                int dado4 = dado.Next(1, 7);
-                int total = dado3 + dado4;
+                contador++;
+                int dado1 = dado.Next(1, 7);
+                int dado2 = dado.Next(1, 7);
+                int total = dado1 + dado2;
                 int acumulado = s + total;
 
-                Console.WriteLine("Dado1 = " + dado3);
-                Console.WriteLine("Dado2 = " + dado4);
+                Console.WriteLine("Dado1 = " + dado1);
+                Console.WriteLine("Dado2 = " + dado2);
+                Console.WriteLine("Puntaje total = " + acumulado);
 
                 s = acumulado;
 
-                if (acumulado <= 100 || ((dado3 == 1) && (dado4 == dado3))) { Console.WriteLine("Ha perdido");
-                    break;
-                }
-                int a = 0;
-                while(dado3 == dado1)
+                if (acumulado >= 100)
                 {
-                    a++;
-                }
-                if (a >= 3) { Console.WriteLine("Ha ganado");
+                    Console.WriteLine("Ha ganado porque su puntaje llegó a 100");
                     break;
                 }
-                if (total >= 6) contador2++;
-                contador++;
-            }Console.WriteLine("Fin del juego");
-            double tan = contador2 / contador;
-            Console.WriteLine("Su porcentaje cuando saco 6 =" + tan);
+
+                if (((dado1 == 1) && (dado2 == dado1)))
+                {
+                    Console.WriteLine("Ha perdido porque ha sacado 1 en los dos dados");
+                    break;
+                }
+                if (dado1 == dado2) a ++;
+                if (a >= 3)
+                {
+                    Console.WriteLine("Ha ganado por sacar 3 pares consecutivos");
+                    break;
+                }
+                if (dado1 != dado2) a = 0;
+
+                if (total >= 6)
+                    contador2 ++;
+                
+                Console.WriteLine("¿Desea continuar?");
+                respuesta = (Console.ReadLine());
+            }
+            double tan = ((double)contador2 / contador) * 100;
+            Console.WriteLine("Su porcentaje cuando saco mas de 6 = " +  tan);
+            Console.WriteLine("Fin del juego");
 
         }
     }
